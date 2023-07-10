8a5a1e5facd73351b9657687d8b58bac9b290e75
Erica Sanchez <ericazach@gmail.com>
2023-07-10 10:51:39 +0200

    add images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="h-28 bg-[#dfd2c7] text-white flex gap-6 justify-center items-center">
-      <Link className="text-2xl" to="/a-nossa-historia">
-        A Nossa Historia
-      </Link>
-      <Link className="text-2xl" to="/">
-        O Casamiento
-      </Link>
-      <Link className="text-2xl" to="/lista-de-presentes">
-        Lista de Presentes
-      </Link>
+    <div className="h-60 bg-[#dfd2c7] text-white flex flex-col gap-6 justify-center items-center">
+      <img className="h-32" src="/images/AnnaPedro.png" alt="annaPedro" />
+      <div className="flex gap-6">
+        <Link className="text-2xl" to="/a-nossa-historia">
+          A Nossa Historia
+        </Link>
+        <Link className="text-2xl" to="/">
+          O Casamiento
+        </Link>
+        <Link className="text-2xl" to="/lista-de-presentes">
+          Lista de Presentes
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex m-10">
+      <div>
+        <h1 className="text-4xl">
+          Bem vindos ao site de celebração de nossa união
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab a,
+          dolorem nesciunt natus facere necessitatibus obcaecati perspiciatis
+          voluptatem esse rerum! Quos minus odio suscipit corporis doloremque
+          quas fuga. Nesciunt, quae.
+        </p>
+      </div>
+      <div className=" w-full">
+        <img
+          className="max-h-[500px] absolute"
+          src="/images/foto1.jpg"
+          alt="foto1"
+        />
+        <img
+          className="max-h-[300px] relative top-80 left-60"
+          src="/images/foto2.jpg"
+          alt="foto2"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
